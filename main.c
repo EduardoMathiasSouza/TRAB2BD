9642ef6e28327b99026f3ef70c6cf4fd1dfd46cd
EduardoMathiasSouza <eduardomsouza@ufpr.br>
2022-04-07 22:28:40 -0300

    Finalizado trabalho de BD

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ struct Transacao
 };
 
 void adiciona_linha(struct Transacao *linha,int it, int t, char acao, char variavel){
-    printf("ITERACAO: %d\n", it);
     linha[it].t = t;
     linha[it].acao = acao;
     linha[it].variavel = variavel;
@@ -46,10 +45,51 @@ int _maior_transa(struct Transacao *linha, int it, int inicio_transacao, int qnt
     return maior_transa;
 }
 
+int testa_ciclo(struct Graph* Grafo, int v, int visitou[], int *path)
+{
+    if (visitou[v] == -1)
+    {
+        visitou[v] = 1;
+        path[v] = 1;
+        struct node* temp = Grafo->adjLists[v];
+        while (temp)
+        {
+            if (visitou[temp->vertex] == -1 && testa_ciclo(Grafo, temp->vertex, visitou, path))
+                return 1;
+            else if (path[temp->vertex])
+                return 1;
+            temp = temp->next;
+        }
+    }
+    path[v] = 0;
+    return 0;
+}
+
+int detecta_ciclo(struct Graph *Grafo, int V)
+{
+    int *visitou = (int*)malloc(V * sizeof(int));
+    int *path = (int*)malloc(V * sizeof(int));
+    int i;
+    for (i = 0; i < V; i++)
+    {
+        visitou[i] = -1;
+        path[i] = 0;
+    }
+    for (i = 0; i < V; i++)
+        if (visitou[i] == -1 && testa_ciclo(Grafo, i, visitou, path))
+            return 1;
+    return 0;
+}
+
+void imprime_transacoes(int transacao_inicial,int transacao_final){
+    for(int i = transacao_inicial; i < transacao_final; i++)
+        fprintf(stdout, "%d,", i);
+    fprintf(stdout, "%d ", transacao_final);
+}
+
 void monta_serialibilidade(struct Transacao *linha, int it, int inicio_transacao,int qntd_de_transacao_ativas){
     int transacao_inicial = _menor_transa(linha, it, inicio_transacao,qntd_de_transacao_ativas);
     int transacao_final = _maior_transa(linha, it, inicio_transacao, qntd_de_transacao_ativas);
-    printf("Menor: %d, Maior: %d\n", transacao_inicial, transacao_final);
     struct Graph *Grafo = createAGraph(transacao_final+1);
     for(int i = inicio_transacao; i < it; i++){
         int transacao_atual = linha[i].t;
@@ -68,7 +108,11 @@ void monta_serialibilidade(struct Transacao *linha, int it, int inicio_transacao
             }
         }
     }
-    printGraph(Grafo);
+    imprime_transacoes(transacao_inicial, transacao_final);
+    if(detecta_ciclo(Grafo, Grafo->numVertices))
+        fprintf(stdout, "NS");
+    else
+        fprintf(stdout, "SS");
 }
 
 int conta_variaveis(struct Transacao *linha, int it, int inicio_transacao, char *variaveis){
@@ -103,16 +147,11 @@ int monta_visao(struct Transacao *linha, int it, int inicio_transacao,char varia
     int serializavel = 1;
     char *variaveis = (char *) malloc (sizeof(char) *it);
     int n_variaveis = conta_variaveis(linha,it,inicio_transacao,variaveis) + 1;
-    printf("NUM VARIAVEIS:%d\n", n_variaveis);
     for(int i = 0; i < n_variaveis; i++){
         int pos_atual = acha_ultimo_write(linha, it, inicio_transacao,variaveis[i]);
-        printf("POS_ATUAL:%d\n", pos_atual);
-        printf("VARIAVEL ATUAL: %c\n", variaveis[i]);
         for(int j = pos_atual; j > inicio_transacao - 1; j--){
             int tipo_atual = linha[pos_atual].t;
-            printf("TIPO_ATUAL:%d\n", tipo_atual);
             if(linha[j].acao == 'R' && linha[j].t == tipo_atual && linha[j].variavel == variaveis[i]){
-                printf("ENTREI");
                 for(int k = j+1; k < pos_atual; k++){
                     if(linha[k].t != linha[j].t && linha[k].acao == 'W' && linha[k].variavel == variaveis[i]){
                         serializavel = 0;
@@ -133,18 +172,20 @@ int main(){
     char variavel;
     int qntd_de_transacao_ativas = 0;
     int inicio_transacao = 1;
-
+    int count = 1;
     while(fscanf(stdin, "%d  %d  %c  %c", &it, &t, &acao, &variavel) != EOF){
         if(t > qntd_de_transacao_ativas)
             qntd_de_transacao_ativas++;
         adiciona_linha(linha,it,t,acao,variavel);
         if(acao == 'C'){
             if(verifica_commits(linha,it, qntd_de_transacao_ativas)){
+                fprintf(stdout, "%d ", count);
                 monta_serialibilidade(linha, it, inicio_transacao, qntd_de_transacao_ativas);
                 if (monta_visao(linha, it, inicio_transacao, qntd_de_transacao_ativas))
                     fprintf(stdout," SV\n");
                 else
                     fprintf(stdout, " NV\n");
+                count++;
                 inicio_transacao = it+1;
                 qntd_de_transacao_ativas = 0;
             }
